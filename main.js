cfca0ad2c0b487de68659b57ed568534f89fc8b1
yusuke-suehiro <63089903+yusuke-suehiro@users.noreply.github.com>
2020-08-06 16:10:27 +0900

    データ格納未時実装

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.bgColor = "#FFFFEE";  //webの背景色を変更
 
 let CrewCode=['HF727','EA283','GUEST'];
 let CrewName=['末廣勇祐','松下海','guest'];
+let CrewNameAlpha=['YusukeSuehiro','UmiMatsushita','guest'];
+
 var AllNumber=3;  //人数
 var data=new Array(3);
 
@@ -39,7 +41,7 @@ function func1() {
     if (Flag == 1) {
       input_message = "";
       document.getElementById("hint_message").innerHTML = input_message;
-      window.location.href = "index2.php";
+      window.location.href = "index2.html";
 
     }
     else {
@@ -78,7 +80,7 @@ var array1 =
   [CrewName[CrewNumber],CrewCode[CrewNumber],"OK"]
 
 ];
-*/
+
 function sheet_to_workbook(sheet, opts) {
   var n = opts && opts.sheet ? opts.sheet : "Sheet1";
   var sheets = {}; sheets[n] = sheet;
@@ -93,7 +95,7 @@ function s2ab(s) {
   for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
     return buf;
   }
-  /*
+
 function func4() {
 var write_opts = {
   type: 'binary'
@@ -104,6 +106,11 @@ var blob = new Blob([s2ab(wb_out)], { type: 'application/octet-stream' });
 saveAs(blob, 'myExcelFile.xlsx');
 }
 */
+function func4() {
+  var ExcelApp = new ActiveXObject( "Excel.Application" );
+  ExcelApp.Visible = true;
+}
+
 //ここまで
 /*
 function func3() {
@@ -156,6 +163,104 @@ function func3() {
 }
 
 */
+
+function func5() {
+    var yourname= document.getElementById("YourName").value;
+    var yourpass= document.getElementById("YourPass").value;
+    var input_message= document.getElementsByClassName("holiday");
+    var input_message_time= document.getElementsByClassName("MonTimes");
+    var flag_mon=0;
+    var flag_write=0;//0が出勤,1が休み
+    var flag_password=0;
+    var mySchedule=["Name","id","Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+    var weekName=["月","火","水","木","金","土","日"];
+    var flag_time=new Array(13);
+    var flag_count=0;
+
+    for (let num=0;num<CrewCode.length;num++) {
+      if (CrewCode[num] === yourpass) {
+        if (CrewName[num] === yourname) {
+          CrewNumber=num;
+          flag_password=1;
+      }
+    }
+    }
+    if (flag_password==0) {
+      document.getElementById("error_message").innerHTML ="名前か従業員コードが間違っています";
+      return;
+    }
+    if (flag_password==1) {
+      mySchedule[0]=CrewNameAlpha[CrewNumber];
+      mySchedule[1]=CrewCode[CrewNumber];
+    }
+
+    for (let day=0;day<7;day++) {
+    //休み希望と時間を設定していた場合
+    flag_count=0;
+    flag_time=[0,0,0,0,0,0,0,0,0,0,0,0,0];
+    flag_mon=0;
+    if (input_message[day].checked == true) {
+      for (let i=day*14+0;i<=day*14+13;i++) {
+          if (input_message_time[i].checked == true) {
+            document.getElementById("error_message").innerHTML = weekName[day]+ "曜日が正しくありません" ;
+            document.getElementById("finish_message").innerHTML = "" ;
+            return;
+          }
+      }
+      flag_write=1;
+    }
+    //休み希望でなく時間を1個以下または3個以上指定している場合
+    if (input_message[day].checked == false) {
+      for (let i=day*14+0;i<=day*14+13;i++) {
+          if (input_message_time[i].checked == true) {
+            flag_mon=flag_mon+1;
+            flag_time[flag_count]=input_message_time[i].value;
+            flag_count=flag_count+1;
+          }
+      }
+      if (flag_mon <= 1){
+        document.getElementById("error_message").innerHTML = weekName[day]+ "曜日が正しくありません" ;
+        //document.getElementById("error_message").innerHTML =input_message_time[0].value;
+        document.getElementById("finish_message").innerHTML = "" ;
+        return;
+      }
+      if (3 <= flag_mon){
+        document.getElementById("error_message").innerHTML = weekName[day]+ "曜日が正しくありません" ;
+        document.getElementById("finish_message").innerHTML = "" ;
+        return;
+      }
+      flag_write=0;
+    }
+
+    if (flag_write == 1){
+      mySchedule[day+2]="O";
+    }
+    if (flag_write == 0) {
+      mySchedule[day+2]=flag_time[0]+"-"+flag_time[1];
+    }
+
+    document.getElementById("error_message").innerHTML = "" ;
+    //document.getElementById("finish_message").innerHTML = mySchedule[2] ;
+
+}
+    document.getElementById("finish1_message").innerHTML = mySchedule[2] ;
+    document.getElementById("finish2_message").innerHTML = mySchedule[3] ;
+    document.getElementById("finish3_message").innerHTML = mySchedule[4] ;
+    document.getElementById("finish4_message").innerHTML = mySchedule[5] ;
+    document.getElementById("finish5_message").innerHTML = mySchedule[6] ;
+    document.getElementById("finish6_message").innerHTML = mySchedule[7] ;
+    document.getElementById("finish7_message").innerHTML = mySchedule[8] ;
+    document.getElementById("finish8_message").innerHTML = mySchedule[0] ;
+    document.getElementById("finish9_message").innerHTML = mySchedule[1] ;
+
+      //document.getElementById("finish_message").innerHTML = input_message[0].checked;
+
+
+
+
+  }
+
+
 //タブの操作
   //クルー専用
   NonActive[0].onclick = function() {
